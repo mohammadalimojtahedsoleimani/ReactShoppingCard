8578bba337c6d8821007540da75aff555e44ca23
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-27 00:41:37 +0330

    loading gif added

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,14 +1,14 @@
 import React , { useContext } from 'react';
 import { ProductsContext } from "../context/ProductContextProvider";
 import Product from "./Shared/Product";
-
+import Spinner from "../assets/icons/Spinner-1s-200px.gif"
 const Store = () => {
 
     const products = useContext ( ProductsContext );
     return (
         <div style={ { display : "flex" , flexWrap : "wrap" , justifyContent : "space-between" } }>
             {
-                products.length === 0 && <p>Loading</p>
+                products.length === 0 && <img src={Spinner} alt="AXE"/>
             }
             {
                 products.map ( item => <Product key={ item.id } productData={ item }/> )
